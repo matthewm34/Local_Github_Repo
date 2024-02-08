f2f8c3da90386c5e2d6f5fdc0412a270ffe234d5
Jeongwoo <jwcho0609@gmail.com>
2024-02-08 13:10:03 -0500

    launch file update

--- launch/lab2_launch.py
+++ launch/lab2_launch.py
@@ -1,14 +1,17 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('~/dontTouch_ws/turtlebot3/turtlebot3_bringup'),
-                         'launch/camera_robot.launch.py')
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('~/dontTouch_ws/turtlebot3/turtlebot3_bringup'),
+                            'launch/camera_robot.launch.py')
+            )
         ),
         Node(
             package='lab2',
