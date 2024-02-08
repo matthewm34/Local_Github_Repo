c179fca6bf76d5550a81461ebc0c13fa681d75ff
jwcho0609 <jwcho0609@gmail.com>
2024-02-07 21:16:18 -0500

    launch file added

---
+++ launch/lab2_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lab2',
+            executable='camera_debugger',
+        ),
+        Node(
+            package='lab2',
+            executable='find_object',
+        ),
+        Node(
+            package='lab2',
+            executable='rotate_tobot',
+        )
+    ])
