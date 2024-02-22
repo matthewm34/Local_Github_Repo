129699858660238077cdca75ca78c457def06f90
Jeongwoo <jwcho0609@gmail.com>
2024-02-22 11:42:48 -0500

    update launch file

---
+++ launch/lab3_launch.py
@@ -0,0 +1,27 @@
+# ME 7785 Lab 2
+# Authors: Jeongwoo Cho, Matthew McKenna
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('turtlebot3_bringup'),
+                            'launch/camera_robot.launch.py')
+            )
+        ),
+        Node(
+            package='lab3',
+            executable='find_object',
+        ),
+        Node(
+            package='lab3',
+            executable='get_object_range',
+        )
+    ])
