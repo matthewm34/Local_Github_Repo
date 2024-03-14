d97bed7c4f0ee1286fd8d4c4d0ee028f38402f1f
Jeongwoo <jwcho0609@gmail.com>
2024-03-14 14:17:27 -0400

    fix indexing in min value

--- turtle_ws/src/lab4/lab4/get_object_range.py
+++ turtle_ws/src/lab4/lab4/get_object_range.py
@@ -81,7 +81,7 @@ class GetObjectRange(Node):
         if min(masked_lidar) == 3:
             ind_max = 100
         else:  
-            ind_max = np.where(masked_lidar == min(masked_lidar))[0]
+            ind_max = np.where(masked_lidar == min(masked_lidar))[0][0]
 
         msg_pos = Point()
         msg_pos.x = float(ind_max)
